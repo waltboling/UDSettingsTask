736f45f64f1ba268fa163e0eef251a7933825135
Walt Boling <bolingjw@gmail.com>
2018-04-26 18:39:11 -0400

    builds a simple app wherein the user can set some basic settings and see the value of those settings represented in 3 text fields

--- UDSettingsTask/ViewController.swift
+++ UDSettingsTask/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var listName: UITextField!
+    @IBOutlet weak var sliderValue: UITextField!
+    @IBOutlet weak var enabledValue: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let userDefaults = UserDefaults.standard
+        //let settingsSlider =
+        let toggleButton = userDefaults.bool(forKey: "Enabled")
+        let sliderString = String(describing: userDefaults.float(forKey: "Slider"))
+        
+        listName.text = userDefaults.object(forKey: "ListName") as? String
+        sliderValue.text = sliderString
+        
+        if toggleButton == true {
+            return enabledValue.text = "On"
+        } else {
+            return enabledValue.text = "Off"
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
